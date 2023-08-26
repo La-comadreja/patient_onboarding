af8b9f024f234331e1f6b10f2a1a9d1d430be41a
Rebecca Sealfon <rebecca.sealfon@gmail.com>
2023-08-26 16:59:05 -0400

    send view to basic url

--- patient_onboarding/urls.py
+++ patient_onboarding/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('onboard/', include('onboard.urls')),
+    path('', include('onboard.urls')),
     path('admin/', admin.site.urls),
 ]
